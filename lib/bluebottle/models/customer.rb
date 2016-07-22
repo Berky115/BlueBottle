5c7d72ea477a9f669579479c17ed91997567e29b
Andrew Facchiano <andrewfacchiano@gmail.com>
2016-07-21 20:17:43 -0700

    Model changes [Drew]
    implementing subscription model based on whiteboard design

--- lib/bluebottle/models/customer.rb
+++ lib/bluebottle/models/customer.rb
@@ -6,13 +6,19 @@ module BlueBottle
       attr_accessor :id,
                     :first_name,
                     :last_name,
-                    :email
+                    :email,
+                    :subscribers
 
       def initialize(id, first_name, last_name, email)
         @id = id
         @first_name = first_name
         @last_name = last_name
         @email = email
+        @subscribers = Hash.new
+      end
+
+      def subscriptions
+        @subscribers
       end
 
       def full_name
--- lib/bluebottle/models/subscription.rb
+++ lib/bluebottle/models/subscription.rb
@@ -1,1 +1,28 @@
-# implement this
+require 'active_support/all'
+
+module BlueBottle
+  module Models
+    class Subscription
+      attr_accessor :coffee,
+                    :customers
+
+      def initialize(coffee)
+      	@coffee = coffee
+        @customers = Array.new
+      end
+
+      def coffee
+        @coffee
+      end
+
+      def customers
+        @customers
+      end
+
+      def add_subscriber(customer, status) 
+      	@customers[customer] = status
+      end
+
+    end
+  end
+end
